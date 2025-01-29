6afb3a3c75dd6e4898a65d67f82a7c12b913942b
IAmJamesBirch <143468315+IAmJamesBirch@users.noreply.github.com>
2025-01-29 12:20:23 -0500

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,6 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+for row in range(5):
+	for col in range(5):
+		x=row
+		y=col
+		z=0.5
+		length=1
+		width=1
+		height=1
+		for i in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+			z+=height
+			length *= 0.9
+			width *= 0.9
+			height *= 0.9
 pyrosim.End()
+
 
