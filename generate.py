6c604f9bcc5ffd51c61bf07acf9f3afd53ba7f71
IAmJamesBirch <143468315+IAmJamesBirch@users.noreply.github.com>
2025-03-17 16:03:04 -0400

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import random
 import pyrosim.pyrosim as pyrosim
 
 def Create_World():
@@ -21,10 +22,11 @@ def Generate_Brain():
 	pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")	
 	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
-	pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = - 1.5 )
-	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.5 )
-	pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1.0 )
+	
+	for i in range(0,3):
+		for j in range(3,5):
+			pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = random.uniform(-1,1))
+
 	pyrosim.End()
 	 
 Create_World()
